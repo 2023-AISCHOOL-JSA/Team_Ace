6d79d8e5782c594566771513b7cc9fd659cdca82
rlaa54 <rlaa54321@gmail.com>
2023-08-03 14:12:38 +0900

    베스트상품 폼

---
+++ DB_script/productexercisequery.sql
@@ -0,0 +1,45 @@
+select * from product;
+
+insert into product(
+	productNum,
+    productName,
+    categoryNum,
+    productDetail,
+    productPrice,
+    productStart,
+    productEnd,
+    sellerCode,
+    uptoDate
+) values(
+	'productnumtest',
+    'productnametest',
+    'categorynumtest',
+    'productdetailtest',
+    'productpricetest',
+	'2023-08-03',
+    '2023-08-05',
+    'sellercodetest',
+    '2023-08-03'
+);
+
+insert into product(
+	productNum,
+    productName,
+    categoryNum,
+    productDetail,
+    productPrice,
+    productStart,
+    productEnd,
+    sellerCode,
+    uptoDate
+) values(
+	'productnumtest2',
+    'productnametest2',
+    'categorynumtest2',
+    'productdetailtest2',
+    'productpricetest2',
+	'2023-06-01',
+    '2023-07-28',
+    'sellercodetest2',
+    '2023-06-01'
+);
