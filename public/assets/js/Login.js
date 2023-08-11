7d2cbd6cfd3cc8d4543429d27c64167db853a5d5
anna931124 <121926567+anna931124@users.noreply.github.com>
2023-08-11 16:36:49 +0900

    login_2
    back 연결 필요 js 참고

--- public/assets/js/Login.js
+++ public/assets/js/Login.js
@@ -1,16 +1,21 @@
 const errMsg = document.getElementById('errMsg'); 
-const inputID= document.getElementsByName('inputId');
-const inputPW= document.getElementsByName('inputPw');
-const login= document.getElementsByName('loginButton');
-        
-// if(iputID !='id' || inputPW!='pw'){
-//     login.addEventListener('click',()=>{
-//         errMsg.setAttribute('display','block')
-//     })
-// }
+const inputID= document.getElementById('inputId');
+const inputPW= document.getElementById('inputPw');
+const loginButton= document.getElementById('loginButton');
+const loginForm= document.getElementById("loginForm");
 
-// addEventListener.
-// document.getElementById('errMsg').setAttribute('display', 'block'); 
+/* db에 담겨있는 'ID' 와 사용자의 입력값, db에 담겨있는 'PW'와 사용자의 입력값
+이 다르면 오류메시지가 뜨는 로직 
 
-// errMsg.classList.remove('errMsg');
-// errMsg.classList.add('errMsg');
+Back 추가 --> 아이디와 비밀번호가 일치하면 로그인된 페이지로 넘어가기 
+*/ 
+
+document.addEventListener('DOMContentLoaded',()=>{
+    loginForm.addEventListener('submit',(e)=>{
+        e.preventDefault();
+        if(inputID.value !=='ID' || inputPW.value !=='PW'){
+            errMsg.style.display='block';
+        }
+        else{errMsg.style.display='none'}
+    })
+})
